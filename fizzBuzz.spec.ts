98fe15f08c3154a34df8a59b210de4c8b173338b
Benjamin Alcade <alcade.benjamin@gmail.com>
2023-10-24 18:01:33 +0200

    fizzBuzz TDD

--- fizzBuzz.spec.ts
+++ fizzBuzz.spec.ts
@@ -1,5 +1,29 @@
 import fizzBuzz from "./fizzBuzz";
 
 test("should return 1", () => {
-  expect(fizzBuzz()).toBe(1);
+  expect(fizzBuzz(1,1)).toBe("1");
 });
+test("should return 2", () => {
+  expect(fizzBuzz(2,2)).toBe("2");
+});
+test("should return fizz", () => {
+  expect(fizzBuzz(3,3)).toBe("fizz");
+});
+test("should return fizz", () => {
+  expect(fizzBuzz(5,5)).toBe("buzz");
+});
+test("should return fizz", () => {
+  expect(fizzBuzz(6,6)).toBe("fizz");
+});
+test("should return fizzBuzz", () => {
+  expect(fizzBuzz(15,15)).toBe("fizzBuzz");
+});
+test("should return fizzBuzz", () => {
+  expect(fizzBuzz(30,30)).toBe("fizzBuzz");
+});
+test("should return 12", () => {
+  expect(fizzBuzz(1,2)).toBe("12");
+});
+// test("should return 12fizz", () => {
+//     expect(fizzBuzz(1,3)).toBe("12fizz");
+//   });
--- fizzBuzz.ts
+++ fizzBuzz.ts
@@ -1,3 +1,16 @@
-export default function fizzBuzz() {
+export default function fizzBuzz(minNumber: number, maxNumber: number) {
+  const minComparedNumber = compareNumbers(minNumber);
 
+  if (minNumber < maxNumber) {
+    const maxComparedNumber = compareNumbers(maxNumber);
+    return minComparedNumber.toString() + maxComparedNumber.toString();
+  }
+  return minComparedNumber.toString();
+}
+
+function compareNumbers(number: number) {
+  if (number % 15 === 0) return "fizzBuzz";
+  if (number % 5 === 0) return "buzz";
+  if (number % 3 === 0) return "fizz";
+  return number;
 }
